c018f4abe5701f91a71808b3900d4d672e6ae122
annaklimenko1 <151486558+annaklimenko1@users.noreply.github.com>
2024-12-25 15:18:01 +0700

    Add files via upload

---
+++ Frontend сайта по макету/myjs/main.js
@@ -0,0 +1,17 @@
+//function Ant(crslId) {
+
+	//let id = document.getElementById(crslId);
+	//if(id) {
+	//	this.crslRoot = id
+	//}
+	//else {
+	//	this.crslRoot = document.querySelector('.ant-carousel')
+	//};
+
+	// Carousel objects
+	//this.crslList = this.crslRoot.querySelector('.ant-carousel-list');
+	//this.crslElements = this.crslList.querySelectorAll('.ant-carousel-element');
+	//this.crslElemFirst = this.crslList.querySelector('.ant-carousel-element');
+	//this.leftArrow = this.crslRoot.querySelector('div.ant-carousel-arrow-left');
+	//this.rightArrow = this.crslRoot.querySelector('div.ant-carousel-arrow-right');
+	//this.indicatorDots = this.crslRoot.querySelector('div.ant-carousel-dots');
